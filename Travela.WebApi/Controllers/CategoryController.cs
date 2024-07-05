bf5cb96e9a6c3d772ab6d4a8112ec1adc8e8b9ec
Batuhan YALIN <batuhanyalin2@gmail.com>
2024-07-05 14:46:01 +0300

    DestinationController oluşturuldu

--- Travela.WebApi/Controllers/CategoryController.cs
+++ Travela.WebApi/Controllers/CategoryController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Travela.BusinessLayer.Abstract;
+using Travela.EntityLayer.Concrete;
 
 namespace Travela.WebApi.Controllers
 {
@@ -14,12 +15,35 @@ namespace Travela.WebApi.Controllers
         {
             _categoryService = categoryService;
         }
-        [Route("CategoryList")]
         [HttpGet]
-        public IActionResult CategoryList()
+        public IActionResult ListCategory()
         {
-            var values=_categoryService.TGetListAll();
+            var values = _categoryService.TGetListAll();
             return Ok(values);
         }
+        [HttpPost]
+        public IActionResult CreateCategory(Category category)
+        {
+            _categoryService.TInsert(category);
+            return Ok("Kategori ekleme işlemi başarıyla tamamlandı.");
+        }
+        [HttpDelete]
+        public IActionResult DeleteCategory(int id)
+        {
+            _categoryService.TDelete(id);
+            return Ok("Kategori silme işlemi başarıyla tamamlandı.");
+        }
+        [HttpGet("GetCategory")] //Aynı attribute u kullanabilmek için [HttpGet("GetCategory")] kullanılır. Burada get attributeunu bu metot için işaretliyoruz.
+        public IActionResult GetCategory(int id)
+        {
+            var values = _categoryService.TGetById(id);
+            return Ok(values);
+        }
+        [HttpPut]
+        public IActionResult UpdateCategory(Category category)
+        {
+            _categoryService.TUpdate(category);
+            return Ok("Kategori güncelleme işlemi başarıyla tamamlandı.");
+        } 
     }
 }
---
+++ Travela.WebApi/Controllers/DestinationController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Travela.BusinessLayer.Abstract;
+using Travela.EntityLayer.Concrete;
+
+namespace Travela.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DestinationController : ControllerBase
+    {
+        private readonly IDestinationService  _destinationService;
+
+        public DestinationController(IDestinationService destinationService)
+        {
+            _destinationService = destinationService;
+        }
+
+        [HttpGet]
+        public IActionResult ListDestination()
+        {
+           var values= _destinationService.TGetListAll();
+            return Ok(values);
+        }
+        [HttpGet("GetDestination")]
+        public IActionResult GetDestination(int id)
+        {
+            var values = _destinationService.TGetById(id);
+            return Ok(values);
+        }
+        [HttpPut]
+        public IActionResult UpdateDestination(Destination destination)
+        {
+            _destinationService.TUpdate(destination);
+            return Ok("Kategori güncelleme işlemi başarıyla tamamlandı.");
+        }
+        [HttpPost]
+        public IActionResult CreateDestination(Destination destination)
+        {
+            _destinationService.TInsert(destination);
+            return Ok("Kategori ekleme işlemi başarıyla tamamlandı.");
+        }
+        [HttpDelete]
+        public IActionResult DeleteDestination(int id)
+        {
+            _destinationService.TDelete(id);
+            return Ok("Kategori silme işlemi başarıyla tamamlandı.");
+        }
+
+    }
+}
