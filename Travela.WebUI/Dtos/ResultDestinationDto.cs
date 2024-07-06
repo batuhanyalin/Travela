a1b87359c0ed2bdcc91a5ada71287883d03d184e
Batuhan YALIN <batuhanyalin2@gmail.com>
2024-07-06 13:59:38 +0300

    ListDestination veiwi oluşturuldu.

--- Travela.WebUI/Dtos/ResultDestinationDto.cs
+++ Travela.WebUI/Dtos/ResultDestinationDto.cs
@@ -9,7 +9,7 @@
         public int CountDay { get; set; }
         public string SubTitle { get; set; }
         public string Description { get; set; }
-        public int Price { get; set; }
-        public string Date { get; set; }
+        public decimal Price { get; set; }
+        public DateTime Date { get; set; }
     }
 }
